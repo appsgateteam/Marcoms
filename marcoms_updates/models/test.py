78d45dcad6e3cd8bc0ef594dcfffcc0e0fb9682e
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-08-27 10:36:14 +0400

    edit advance to come as invoice in slaes order module

--- marcoms_updates/models/test.py
+++ marcoms_updates/models/test.py
@@ -3,6 +3,7 @@ from odoo.exceptions import except_orm, ValidationError ,UserError
 import odoo.addons.decimal_precision as dp
 from datetime import datetime, timedelta , date
 import math
+import time
 from num2words import num2words
 from odoo.exceptions import Warning
 from odoo.tools import float_utils, float_compare ,pycompat ,email_re, email_split, email_escape_char, float_is_zero, date_utils
@@ -610,7 +611,62 @@ class Uom(models.Model):
 
     show_sale = fields.Boolean('To Show in Sales')
 
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
     
+    @api.multi
+    def create_invoices(self):
+        sale_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
+
+        if self.advance_payment_method == 'delivered':
+            sale_orders.action_invoice_create()
+        elif self.advance_payment_method == 'all':
+            sale_orders.action_invoice_create(final=True)
+        else:
+            # Create deposit product if necessary
+            if not self.product_id:
+                vals = self._prepare_deposit_product()
+                self.product_id = self.env['product.product'].create(vals)
+                self.env['ir.config_parameter'].sudo().set_param('sale.default_deposit_product_id', self.product_id.id)
+            invoicename = ''
+            sale_line_obj = self.env['sale.order.line']
+            for order in sale_orders:
+                if self.advance_payment_method == 'percentage':
+                    amount = order.amount_untaxed * self.amount / 100
+                    invoicename = """Invoice %s %: %s""" % (self.amount,time.strftime('%m %Y'))
+                else:
+                    amount = self.amount
+                    invoicename = """Invoice : %s""" % (time.strftime('%m %Y'))
+                if self.product_id.invoice_policy != 'order':
+                    raise UserError(_('The product used to invoice a down payment should have an invoice policy set to "Ordered quantities". Please update your deposit product to be able to create a deposit invoice.'))
+                if self.product_id.type != 'service':
+                    raise UserError(_("The product used to invoice a down payment should be of type 'Service'. Please use another product or update this product."))
+                taxes = self.product_id.taxes_id.filtered(lambda r: not order.company_id or r.company_id == order.company_id)
+                if order.fiscal_position_id and taxes:
+                    tax_ids = order.fiscal_position_id.map_tax(taxes, self.product_id, order.partner_shipping_id).ids
+                else:
+                    tax_ids = taxes.ids
+                context = {'lang': order.partner_id.lang}
+                analytic_tag_ids = []
+                for line in order.order_line:
+                    analytic_tag_ids = [(4, analytic_tag.id, None) for analytic_tag in line.analytic_tag_ids]
+                so_line = sale_line_obj.create({
+                    'name': invoicename,
+                    'price_unit': amount,
+                    'product_uom_qty': 0.0,
+                    'order_id': order.id,
+                    'discount': 0.0,
+                    'product_uom': self.product_id.uom_id.id,
+                    'product_id': self.product_id.id,
+                    'analytic_tag_ids': analytic_tag_ids,
+                    'tax_id': [(6, 0, tax_ids)],
+                    'is_downpayment': True,
+                })
+                del context
+                self._create_invoice(order, so_line, amount)
+        if self._context.get('open_invoices', False):
+            return sale_orders.action_view_invoice()
+        return {'type': 'ir.actions.act_window_close'}
 
 
 class SaleOrder_customize(models.Model):
@@ -1036,6 +1092,8 @@ class SaleOrder_customize(models.Model):
             'limit': 80,
             'context': vals,
         }
+
+    
 
     @api.multi
     def create_purchase_requisition_from_sales(self):
