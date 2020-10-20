3e6fa4dfd2efe5c23e6406f08cbeb3c50a7e4b7a
Bincy296 <bincy@apps-gate.net>
2020-10-20 13:12:16 +0000

    hr_leave_revised

---
+++ hr_holidays_accrual_advanced/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2018-2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Advanced Accrual Allocation',
+    'version': '12.0.1.0.2',
+    'category': 'Human Resources',
+    'website': 'https://github.com/OCA/hr',
+    'author':
+        'Brainbean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Advanced accrual leaves allocation',
+    'depends': [
+        'hr_holidays',
+    ],
+    'data': [
+        'security/hr_holidays_accrual_security.xml',
+        'security/ir.model.access.csv',
+        'wizards/hr_leave_allocation_accrual_calculator.xml',
+        'views/hr_leave_allocation.xml',
+        'views/hr_leave_allocation_accruement.xml',
+    ],
+}
