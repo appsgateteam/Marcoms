e32168202798fec41f409bebc8c1f0915fa8c96c
Bincy296 <bincy@apps-gate.net>
2020-10-27 08:09:51 +0000

    modified

--- marcoms_updates/models/test.py
+++ marcoms_updates/models/test.py
@@ -2000,6 +2000,7 @@ class Hrcontractscus(models.Model):
 
     hr_allowance_line_ids = fields.One2many('hr.allowance.line','contract_id',string='HR Allowance')
     hr_total_wage = fields.Float('Total Salary',compute="_total_wage")
+    emp_branch_name = fields.Many2one('employee.category.type', 'Branch Name')
 
     @api.multi
     def _total_wage(self):
@@ -2107,6 +2108,101 @@ class HrPayslipcus(models.Model):
                 if rec.hr_variance_line_id:
                     raise UserError(_('You cannot delete a payslip which have payroll transaction(Variance)!'))
         return super(HrPayslipcus, self).unlink()
+
+    @api.multi
+    def action_payslip_done(self):
+        res = super(HrPayslipcus, self).action_payslip_done()
+
+        for slip in self:
+            line_ids = []
+            debit_sum = 0.0
+            credit_sum = 0.0
+            date = slip.date or slip.date_to
+            currency = slip.company_id.currency_id or slip.journal_id.company_id.currency_id
+
+            name = _('Payslip of %s') % (slip.employee_id.name)
+            move_dict = {
+                'narration': name,
+                'ref': slip.number,
+                'journal_id': slip.journal_id.id,
+                'date': date,
+            }
+            for line in slip.details_by_salary_rule_category:
+                amount = currency.round(slip.credit_note and -line.total or line.total)
+                if currency.is_zero(amount):
+                    continue
+                debit_account_id = line.salary_rule_id.account_debit.id
+                credit_account_id = line.salary_rule_id.account_credit.id
+
+                if debit_account_id:
+                    debit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=False),
+                        'account_id': debit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount > 0.0 and amount or 0.0,
+                        'credit': amount < 0.0 and -amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id or slip.contract_id.analytic_account_id.id,
+                        'emp_branch_name': slip.contract_id.emp_branch_name.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(debit_line)
+                    debit_sum += debit_line[2]['debit'] - debit_line[2]['credit']
+
+                if credit_account_id:
+                    credit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=True),
+                        'account_id': credit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount < 0.0 and -amount or 0.0,
+                        'credit': amount > 0.0 and amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id or slip.contract_id.analytic_account_id.id,
+                        'emp_branch_name': slip.contract_id.emp_branch_name.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(credit_line)
+                    credit_sum += credit_line[2]['credit'] - credit_line[2]['debit']
+
+            if currency.compare_amounts(credit_sum, debit_sum) == -1:
+                acc_id = slip.journal_id.default_credit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Credit Account!') % (
+                        slip.journal_id.name))
+                adjust_credit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': 0.0,
+                    'credit': currency.round(debit_sum - credit_sum),
+                })
+                line_ids.append(adjust_credit)
+
+            elif currency.compare_amounts(debit_sum, credit_sum) == -1:
+                acc_id = slip.journal_id.default_debit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Debit Account!') % (
+                        slip.journal_id.name))
+                adjust_debit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': currency.round(credit_sum - debit_sum),
+                    'credit': 0.0,
+                })
+                line_ids.append(adjust_debit)
+            move_dict['line_ids'] = line_ids
+            move = self.env['account.move'].create(move_dict)
+            slip.write({'move_id': move.id, 'date': date})
+            move.post()
+        return res
+
 
 class HrVarianceLine(models.Model):
     _name = 'hr.variance.line'
@@ -5813,6 +5909,12 @@ class AssetsCus(models.Model):
     _inherit = 'account.asset.asset'
 
     tracking_id = fields.One2many('asset.tracking','asset_name',string="Tracking Line")
+
+class EmployeeCategoryType(models.Model):
+    _name = 'employee.category.type'
+
+    name =  fields.Char('Name')
+    #emp_category_code = fields.Integer('Code')
 
 class PaymentRequest(models.Model):
     _name = 'payment.request'
