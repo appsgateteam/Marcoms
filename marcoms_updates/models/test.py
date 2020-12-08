d3c8e382934c61fda334c4fe1195da80e706a3e2
Bincy296 <bincy@apps-gate.net>
2020-12-08 16:46:05 +0000

    modified timesheet cost

--- marcoms_updates/models/test.py
+++ marcoms_updates/models/test.py
@@ -2095,9 +2095,8 @@ class HRpayrolltranLine(models.Model):
     @api.onchange('number_of_hours')
     def _get_amount(self):
         for rec in self:
-            rec.timesheet_cost = rec.employee_id.contract_id.hr_total_wage
-            time_cost =  ((rec.timesheet_cost /30)/8)
-            rec.allowance = rec.number_of_hours * time_cost
+            rec.timesheet_cost = rec.employee_id.timesheet_cost
+            rec.allowance = rec.number_of_hours * rec.timesheet_cost
 
 class HrSalaryRulecus(models.Model):
     _inherit = 'hr.salary.rule'
