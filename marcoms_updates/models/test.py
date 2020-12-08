f60aed4fcb991d3dff64efd2a752c8bb13f8f0bc
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-12-08 12:41:44 +0000

    Update test.py

--- marcoms_updates/models/test.py
+++ marcoms_updates/models/test.py
@@ -2096,7 +2096,7 @@ class HRpayrolltranLine(models.Model):
     def _get_amount(self):
 
         for rec in self:
-          rec.timesheet_cost = rec.employee_id.contract_id.hr_total_wage
+           rec.timesheet_cost = rec.employee_id.contract_id.hr_total_wage
            time_cost =  ((rec.timesheet_cost /30)/8)
            rec.allowance = rec.number_of_hours * time_cost
 
